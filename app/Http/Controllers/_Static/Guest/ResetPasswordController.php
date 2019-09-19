29ced97b2c1afdb0718c6490072b406a9a8fe92f
stilo bit <stilobit@gmail.com>
2019-09-19 13:45:04 +0000

    setup login html and css page

---
+++ app/Http/Controllers/_Static/Guest/ResetPasswordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\_Static\Guest;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ResetPasswordController extends Controller
+{
+    //
+}
--- resources/views/static/guest/login.blade.php
+++ resources/views/static/guest/login.blade.php
@@ -4,6 +4,15 @@
 
 @section('page-content')
 
-	<div class="login"></div>
+	<div class="login">
+		<form>
+			<input type="text" name="email" placeholder="Email adress">
+			<input type="password" name="password" placeholder="Password">
+			<input type="submit" name="submit" placeholder="Login">
+			<span class="divider"></span>
+			<span>- <a href="#">Register</a></span>
+			<span>- <a href="#">Reset password</a></span>
+		</form>
+	</div>
 
 @endsection
